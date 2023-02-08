e4491df35a9afef671769e29938e4646ce8c3b91
Alex Shen <ashen12@ugradx.cs.jhu.edu>
2023-02-08 01:11:34 -0500

    wrote main functions for c

--- csf_assign02/c_hexmain.c
+++ csf_assign02/c_hexmain.c
@@ -6,6 +6,11 @@ int main(void) {
   // a row will only be printed if 16 characters are produced or CTRLD
   char buf[16];
   hex_read(buf);
+  char *s = "Hello";
+  hex_write_string(s);
+  hex_format_offset(8, buf);
+  hex_format_byte_as_hex(236, buf);
+  s = hex_to_printable(69);
   // TODO
   return 0;
 }
