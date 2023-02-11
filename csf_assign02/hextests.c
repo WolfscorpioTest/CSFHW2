991302cfa93f741e1a0095c8f21b7d7b27f82faa
Alex Shen <ashen12@ugradx.cs.jhu.edu>
2023-02-10 22:41:14 -0500

    added additional tests

--- csf_assign02/hextests.c
+++ csf_assign02/hextests.c
@@ -14,6 +14,7 @@ typedef struct {
 
 // setup function (to create the test fixture)
 TestObjs *setup(void) {
+  
   TestObjs *objs = malloc(sizeof(TestObjs));
   strcpy(objs->test_data_1, "Hello, world!\n");
   return objs;
@@ -50,21 +51,32 @@ int main(int argc, char **argv) {
 void testFormatOffset(TestObjs *objs) {
   (void) objs; // suppress warning about unused parameter
   char buf[16];
+  //testing all different combinations of strings
   hex_format_offset(0x00000001u, buf);
   ASSERT(0 == strcmp(buf, "00000001"));
   hex_format_offset(0xabcd1234u, buf);
   ASSERT(0 == strcmp(buf, "abcd1234"));
+  hex_format_offset(0x00000000u, buf);
+  ASSERT(0 == strcmp(buf,"00000000"));
+  hex_format_offset(0xffffffffu, buf);
+  ASSERT(0 == strcmp(buf,"ffffffff"));
+  hex_format_offset(0xf0f0f0f0u, buf);
+  ASSERT(0 == strcmp(buf,"f0f0f0f0"));
 }
 
 void testFormatByteAsHex(TestObjs *objs) {
+  //strcpy(objs->test_data_1, "Hello, world!\n");
   char buf[16];
   hex_format_byte_as_hex(objs->test_data_1[0], buf);
   ASSERT(0 == strcmp(buf, "48"));
+  hex_format_byte_as_hex(objs->test_data_1[6], buf);
+  ASSERT(0 == strcmp(buf, "20"));
 }
 
 void testHexToPrintable(TestObjs *objs) {
   ASSERT('H' == hex_to_printable(objs->test_data_1[0]));
   ASSERT('.' == hex_to_printable(objs->test_data_1[13]));
+  ASSERT('.' == hex_to_printable(objs->test_data_1[12]));
 }
 
 void testStringLength(TestObjs *objs) {
